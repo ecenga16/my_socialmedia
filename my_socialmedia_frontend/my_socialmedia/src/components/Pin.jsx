bec8dba5da49cdf6169440f9c12d88dc0ce7df27
aldocenga <eraldocenga@nmc.al>
2023-10-06 10:13:05 +0200

    "Implement Masonry layout for pin display"

---
+++ my_socialmedia_frontend/my_socialmedia/src/components/Pin.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { client, urlFor } from '../client';
+
+
+const Pin = ({pin: {postedBy, image, _id, destination}}) => {
+
+    return (
+        <div>
+            <img className="rounded-lg w-full" alt="user-post" src={urlFor(image.width(250).url())}/>
+        </div>
+    )
+
+}
+
+export default Pin;
