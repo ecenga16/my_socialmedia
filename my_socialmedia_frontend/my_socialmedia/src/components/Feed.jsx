dd62ed80db17beae4dde8d165df2bde2727a22d0
aldocenga <eraldocenga@nmc.al>
2023-10-06 09:26:36 +0200

    Update spinner import and usage

--- my_socialmedia_frontend/my_socialmedia/src/components/Feed.jsx
+++ my_socialmedia_frontend/my_socialmedia/src/components/Feed.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
+
+    const [loading, setLoading] = useState(false);
+
+
+    if (loading) return <Spinner message="We are adding new ideas for you!"/>
 
     return (
         <div>
