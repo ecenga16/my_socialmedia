c83b5cc18fc21a9e59a9a3a3114b96af8c9c8e07
aldocenga <eraldocenga@nmc.al>
2023-10-06 14:36:12 +0200

    Implement Pins container component with routing

--- my_socialmedia_frontend/my_socialmedia/src/container/Pins.jsx
+++ my_socialmedia_frontend/my_socialmedia/src/container/Pins.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Routes, Route} from 'react-router-dom';
+import { Navbar, Feed, CreatePin, PinDetail, Search} from '../components'
+import { useState } from "react";
 
 
-const Pins = () => {
 
-    return <div>
+const Pins = ({user}) => {
 
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    return <div className="px-2 md:px-5">
+            <div className="bg-gray-50">
+                <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user}/>
+
+            </div>
+            <div className="h-full">
+                <Routes>
+                    <Route path="/" element={<Feed />}/>
+                    <Route path="/category/:categoryId" element={<Feed />}/>
+                    <Route path="/pin-detail/:pinId" element={<PinDetail />}/>
+                    <Route path="/create-pin" element={<CreatePin user={user} />}/>
+                    <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />}/>
+                </Routes>
+            </div>
     </div>
 };
 
