1b75cfce632518022f4e4e57231f8986f0f4bca5
aldocenga <eraldocenga@nmc.al>
2023-10-06 09:12:38 +0200

    New components (Spinner, MasonaryLayout)

---
+++ my_socialmedia_frontend/my_socialmedia/src/components/MasonaryLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const MasonaryLayout = () => {
+
+    return (
+        <div>
+            Feed
+        </div>
+    )
+
+}
+
+export default MasonaryLayout;
---
+++ my_socialmedia_frontend/my_socialmedia/src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Spinner = () => {
+
+    return (
+        <div>
+            Feed
+        </div>
+    )
+
+}
+
+export default Spinner;
