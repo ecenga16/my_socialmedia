199c75d65d26d9e7d6c6550b1d4d878252909734
aldocenga <eraldocenga@nmc.al>
2023-10-06 10:16:53 +0200

    Restructure multiple schemas

--- my_socialmedia_backend/schemas/pin.js
+++ my_socialmedia_backend/schemas/pin.js
@@ -1,44 +1,57 @@
 export default {
     name: 'pin',
-    title : 'Pin',
+    title: 'Pin',
     type: 'document',
-    fields: [{
+    fields: [
+      {
         name: 'title',
         title: 'Title',
         type: 'string',
-    }
-    ,
+      },
+      {
+        name: 'about',
+        title: 'About',
+        type: 'string',
+      },
+    {
+        name: 'destination',
+        title: 'Destination',
+        type: 'url',
+      },
     {
         name: 'category',
         title: 'Category',
         type: 'string',
-
-    }
-    ,
-    {   
+    },
+    {
         name: 'image',
         title: 'Image',
         type: 'image',
         options: {
-            hotspot: true
-        }
-    } 
-    , 
-    {
+          hotspot: true,
+        },
+      },
+      {
         name: 'userId',
-        title: 'UserID',
+        title: 'UserId',
         type: 'string',
-    }  ,
-    {
+      },
+      {
         name: 'postedBy',
         title: 'PostedBy',
         type: 'postedBy',
-    }  ,
-    {
+      },
+      {
         name: 'save',
         title: 'Save',
         type: 'array',
-        of: [{type: 'save'}]
-    } ,
-]   
-}
+        of: [{ type: 'save' }],
+      },
+      {
+        name: 'comments',
+        title: 'Comments',
+        type: 'array',
+        of: [{ type: 'comment' }],
+      },
+    ],
+  };
