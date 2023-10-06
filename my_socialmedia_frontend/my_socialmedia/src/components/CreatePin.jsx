34914aa90cb4349d089618f5b1d57f7c022c7877
aldocenga <eraldocenga@nmc.al>
2023-10-06 17:04:31 +0200

    Add fields prompt and update CreatePin layout

--- my_socialmedia_frontend/my_socialmedia/src/components/CreatePin.jsx
+++ my_socialmedia_frontend/my_socialmedia/src/components/CreatePin.jsx
@@ -12,7 +12,7 @@ const CreatePin = ({ user }) => {
   const [about, setAbout] = useState('');
   const [loading, setLoading] = useState(false);
   const [destination, setDestination] = useState();
-  const [fields, setFields] = useState();
+  const [fields, setFields] = useState(false);
   const [category, setCategory] = useState();
   const [imageAsset, setImageAsset] = useState();
   const [wrongImageType, setWrongImageType] = useState(false);
@@ -22,7 +22,18 @@ const CreatePin = ({ user }) => {
 
     return (
         <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
-            CreatePin
+            {fields && (
+                <p className='text-red-500 mb-5 text-xl transition-all ease-in duration-150'>
+                    Please fill the fields!
+                </p>
+            )}
+            <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 lg:w-4/5 w-full">
+                <div className=" bg-secondaryColor p-3 flex flex-0.7 w-full">
+                    <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
