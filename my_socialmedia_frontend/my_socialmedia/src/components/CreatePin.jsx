586d7b1e26d9763182ac9177d932fe5b99175a52
ecenga16 <ecenga16@epoka.edu.al>
2023-10-07 10:39:33 +0200

    Implement image upload feature with validation

--- my_socialmedia_frontend/my_socialmedia/src/components/CreatePin.jsx
+++ my_socialmedia_frontend/my_socialmedia/src/components/CreatePin.jsx
@@ -17,8 +17,27 @@ const CreatePin = ({ user }) => {
   const [imageAsset, setImageAsset] = useState();
   const [wrongImageType, setWrongImageType] = useState(false);
 
-  const navigate = useNavigate();
+        const navigate = useNavigate();
 
+        const uploadImage = (e) => {
+            const selectedFile = e.target.files[0];
+            if (selectedFile.type === 'image/png' || selectedFile.type === 'image/svg' || selectedFile.type === 'image/jpeg' || selectedFile.type === 'image/gif' || selectedFile.type === 'image/tiff') {
+            setWrongImageType(false);
+            setLoading(true);
+            client.assets
+                .upload('image', selectedFile, { contentType: selectedFile.type, filename: selectedFile.name })
+                .then((document) => {
+                setImageAsset(document);
+                setLoading(false);
+                })
+                .catch((error) => {
+                console.log('Upload failed:', error.message);
+                });
+            } else {
+            setLoading(false);
+            setWrongImageType(true);
+            }
+        };
 
     return (
         <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
@@ -30,7 +49,45 @@ const CreatePin = ({ user }) => {
             <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 lg:w-4/5 w-full">
                 <div className=" bg-secondaryColor p-3 flex flex-0.7 w-full">
                     <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
-
+                        {loading && <Spinner/>}
+                        {wrongImageType && <p>Wrong Image Type</p>}
+                        {!imageAsset ? (
+                            <label>
+                                <div className="flex flex-col items-center justify-center h-full ">
+                                    <div className="flex flex-col justify-center items-center">
+                                        <p className="font-bold text-2xl">
+                                        <AiOutlineCloudUpload/>
+                                        </p>
+                                        <p className='text-lg'>Click to upload</p>
+                                    </div>
+                                    <p className='mt-32 text-gray-300'>
+                                        Png, jpeg, jpg image types are allowed! 
+                                    </p>
+                                </div>
+                                <input
+                                    type="file"
+                                    name="upload-image"
+                                    onChange={uploadImage}
+                                    className="w-0 h-0"
+                                />
+                            </label>
+                        ) : (
+                            
+                        <div className="relative h-full">
+                            <img
+                            src={imageAsset?.url}
+                            alt="uploaded-pic"
+                            className="h-full w-full"
+                            />
+                            <button
+                            type="button"
+                            className="absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out"
+                            onClick={() => setImageAsset(null)}
+                            >
+                            <MdDelete />
+                            </button>
+                         </div>
+                        )}
                     </div>
                 </div>
             </div>
